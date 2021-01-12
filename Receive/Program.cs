812611f399fcfea02312d7a86a7da595921f61c2
oturkyilmaz <omer.turkyilmaz@hepsiburada.com>
2021-01-12 11:37:04 +0300

    [initialize]

--- Receive/Program.cs
+++ Receive/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
 
 namespace Receive
 {
@@ -6,7 +10,117 @@ namespace Receive
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ömer https://www.c-sharpcorner.com/article/using-net-core-with-rabbitmq-for-async-operations/ 
+
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://dazfzfmg:HLo6r8xAKPluHAqQE94tMj-PgWweHUAt@grouse.rmq.cloudamqp.com/dazfzfmg");
+
+
+            ////factory.HostName = "localhost";
+            ////factory.UserName = "guest";
+            ////factory.Password = "@mAT23ok";
+            ///
+            string queueName = "task_queue";
+            var rabbitMqConnection = factory.CreateConnection();
+            var rabbitMqChannel = rabbitMqConnection.CreateModel();
+
+            rabbitMqChannel.QueueDeclare(queue: queueName,
+                            durable: true,
+                            exclusive: false,
+                            autoDelete: false,
+                            arguments: null);
+
+            rabbitMqChannel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+            int messageCount = Convert.ToInt16(rabbitMqChannel.MessageCount(queueName));
+            Console.WriteLine(" Listening to the queue. This channels has {0} messages on the queue", messageCount);
+
+            var consumer = new EventingBasicConsumer(rabbitMqChannel);
+            consumer.Received += (model, ea) =>
+            {
+
+                var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                Console.WriteLine(" Mesaj Alındı: " + message);
+
+                rabbitMqChannel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+
+
+                
+                Thread.Sleep(1000);
+
+            };
+            rabbitMqChannel.BasicConsume(queue: queueName,
+                                 autoAck: false,
+                                 consumer: consumer);
+
+
+            Console.ReadLine();
+
+
+            //string queueName = "task_queue";
+
+            //var factory = new ConnectionFactory();//instance bağlanmak için oluşturduk
+            //factory.Uri = new Uri("amqps://dazfzfmg:HLo6r8xAKPluHAqQE94tMj-PgWweHUAt@grouse.rmq.cloudamqp.com/dazfzfmg");
+
+
+            ////factory.HostName = "localhost";
+            ////factory.UserName = "guest";
+            ////factory.Password = "@mAT23ok";
+
+            ////bağlantıyı açalım.
+            ////usin ifadesi içerisinde bir clasdan bir nesne oluşturursak 
+            ////ve o nesnemiz idisposable interfacesine sahipse using bloğundan çıktığında bu arkadaş memordiden silinir
+            //using (var connection = factory.CreateConnection())
+            //{
+            //    //bağlantı hazırsa kanal oluşturalım
+            //    using (var channel = connection.CreateModel())
+            //    {
+
+            //        channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false, null);
+
+            //        channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+            //        //prefectSize : 
+            //        //1 : bana bir bir dağıt. 2 dersem iki iki yollar
+            //        //false : her consumer 1 tane queue alabilir.  diyelim 10 tane consumer var. 10 mesaj alır.
+            //        //true olursa 10 consumer toplam da 1 tane mesaj alır. 
+
+
+
+
+            //        var consumer = new EventingBasicConsumer(channel);//bu kanalı dinle
+            //        consumer.Received += (model, ea) =>
+            //        {
+
+            //            var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+            //            Console.WriteLine("Mesaj Aldın" + message);
+
+
+
+
+
+            //            channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);//okundu mesajı
+
+            //            int time = 100;// int.Parse(GetMessages(args));
+            //            Thread.Sleep(time);
+
+
+            //        };
+
+            //        channel.BasicConsume(queue: queueName, autoAck: false, consumer);
+            //        //autoAck : true verirsek doğruda yanlışda işlesek rabbitmq dan silinecek. false dersek biz bilgi vericez öle sil diyecez
+
+
+
+
+            //    }
+
+
+
+            //}
+            //Console.ReadLine();
         }
+
+      
     }
 }
--- Send/Program.cs
+++ Send/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace Send
 {
@@ -6,7 +8,61 @@ namespace Send
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory();//instance bağlanmak için oluşturduk
+            factory.Uri = new Uri("amqps://dazfzfmg:HLo6r8xAKPluHAqQE94tMj-PgWweHUAt@grouse.rmq.cloudamqp.com/dazfzfmg");
+
+            //factory.HostName = "localhost";
+            //factory.UserName = "guest";
+            //factory.Password = "@mAT23ok";
+
+            //bağlantıyı açalım.
+            //using ifadesi içerisinde bir clasdan bir nesne oluşturursak 
+            //ve o nesnemiz idisposable interfacesine sahipse using bloğundan çıktığında bu arkadaş memordiden silinir
+            using (var connection = factory.CreateConnection())
+            {
+                //bağlantı hazırsa kanal oluşturalım
+                using (var channel = connection.CreateModel())
+                {
+                    //şimdi kuyruk oluşturlaım
+                    channel.QueueDeclare(queue: "task_queue",
+                                         durable: true,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+                    //hello = kuyruk ismi.
+                    //durable = false ise rabbitmq mesajı bellekte tutar. ve restart olduğunda uçar
+                    //durable = true ise restart bile olsa diskte tutar birşey olmaz.
+                    //exclusive = bu kuyruğa bir tane mi kanal bağlansın, başka  kanallarda bağlanabilsin mi ?
+                    //autodelete = bir kuyrukta 20 tane mesaj var diyelim. kuyrukta ki mesaj bitince silinsinmi ?
+
+                    string message = "Ömer Türkyılmaz Deneme Mesajı";
+
+                    for (int i = 1; i < 11; i++)
+                    {
+                        var bodyByte = Encoding.UTF8.GetBytes($"{message}-{i}");
+                        var properties = channel.CreateBasicProperties();
+                        properties.Persistent = true;//Mesajıda sağlamaa aldık.
+
+                        //mesajı gönderelim
+                        channel.BasicPublish(exchange: "",
+                                             routingKey: "task_queue",
+                                             basicProperties: properties, 
+                                             body: bodyByte);
+                        //excahnge "" boş gönderdik. Default exchange oluyor.
+                        //routingkey ile kuyruk ismimiz aynı olmalı.
+                        //Default exhange kullanıyorsak route key kuyruk ismi ile aynı olması gerekiyor.
+
+                        Console.WriteLine($"Mesajınız Gönderilmiştir : {message}-{i}");
+                    }
+
+
+
+
+                }
+
+
+                Console.ReadLine();
+            }
         }
     }
 }
